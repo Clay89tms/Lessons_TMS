025fac46444c7c84661ebda3ebb864c96b6b2984
Clay89tms <bboyClay89@gmail.com>
2022-12-22 17:35:01 +0300

    L23 HW Com2 (p3)

--- L22/src/main/java/org/example/IdCarsServlet.java
+++ L22/src/main/java/org/example/IdCarsServlet.java
@@ -5,59 +5,78 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.util.*;
 
 public class IdCarsServlet extends HttpServlet {
 
-    private static Map<String, String> map = new HashMap<>();
+    private static Map<String, CarCreate> map = new HashMap<>();
+    private static CarCreate car;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         String id = req.getParameter("id");
         if (id != null) {
-            resp.getOutputStream().print(map.get(id));
+            resp.getOutputStream().print(
+                    "id = " + map.get(id).getId() +
+                            "\ntype = " + map.get(id).getType() +
+                            "\ncolor = " + map.get(id).getColor() +
+                            "\nprice = " + map.get(id).getPrice()
+            );
         }
 
         if ((id == null) && (!map.isEmpty())) {
             List<String> list = new ArrayList<>();
             list.addAll(map.keySet());
             for (int i = 0; i < list.size(); i++) {
-                resp.getOutputStream().println(list.get(i) + "\t " + map.get(list.get(i)));
+                resp.getOutputStream().println(list.get(i) +
+                        "\t" + map.get(list.get(i)).getType() +
+                        "\t" + map.get(list.get(i)).getColor() +
+                        "\t" + map.get(list.get(i)).getPrice());
+                System.out.println(carToString(map, list, i));
             }
-            System.out.println(map);
         }
+    }
+
+    private String carToString(Map<String, CarCreate> map, List<String> list, int i) {
+        return "[id = " + map.get(list.get(i)).getId() + "\t" +
+                    ", type = " + map.get(list.get(i)).getType() + "\t" +
+                    ", color = " + map.get(list.get(i)).getColor() + "\t" +
+                    ", price = " + map.get(list.get(i)).getPrice() + "]";
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String id = req.getParameter("id");
-        String name = req.getParameter("name");
-        map.put(id, name);
-//        map = req.getParameterMap();
-
+        String type = req.getParameter("type");
+        String color = req.getParameter("color");
+        String price = req.getParameter("price");
+        car = new CarCreate();
+        carSetData(id, type, color, price);
 
         Enumeration<String> parameterNames = req.getParameterNames();
         while (parameterNames.hasMoreElements()) {
             String key = parameterNames.nextElement();
             String value = req.getParameter(key);
-
             System.out.println(key + " " + value);
         }
     }
 
+
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String id = req.getParameter("id");
-        String name = req.getParameter("name");
+        String type = req.getParameter("type");
+        String color = req.getParameter("color");
+        String price = req.getParameter("price");
+        carSetData(id, type, color, price);
+
         if ((id != null) && (!map.isEmpty())) {
             List<String> list1 = new ArrayList<>();
             list1.addAll(map.keySet());
             for (int i = 0; i < list1.size(); i++) {
-                if (list1.get(i).equals(id)){
-                    map.put(id, name);
+                if (list1.get(i).equals(id)) {
+                    map.put(id, car);
                 }
             }
         }
@@ -70,10 +89,19 @@ public class IdCarsServlet extends HttpServlet {
             List<String> list2 = new ArrayList<>();
             list2.addAll(map.keySet());
             for (int i = 0; i < list2.size(); i++) {
-                if (list2.get(i).equals(id)){
+                if (list2.get(i).equals(id)) {
                     map.remove(id);
                 }
             }
         }
+    }
+
+    private Map carSetData(String id, String type, String color, String price) {
+        car.setId(id);
+        car.setType(type);
+        car.setColor(color);
+        car.setPrice(price);
+        map.put(id, car);
+        return map;
     }
 }
