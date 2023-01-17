402881375460ae4a72d6a8f40cd4f061819c8430
Roman <114007909+Clay89tms@users.noreply.github.com>
2023-01-17 08:23:19 +0300

    L28 SOLID initial commit

---
+++ L28_Solid/src/main/java/org/example/Singleton.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Singleton {
+    private static Singleton instance = new Singleton();
+
+    private Singleton() {
+    }
+
+    public static synchronized Singleton getInstance(){
+        if (instance == null){
+            synchronized (Singleton.class){
+                if(instance == null){
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
