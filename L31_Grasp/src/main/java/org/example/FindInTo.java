b6fee8ce6a47073a6a36613d1b0567d11003a1e2
Clay89TMS <bboyClay89@gmail.com>
2023-02-03 09:22:59 +0300

    L31 Grasp Employee commit13

--- L31_Grasp/src/main/java/org/example/FindInTo.java
+++ L31_Grasp/src/main/java/org/example/FindInTo.java
@@ -6,19 +6,17 @@ public class FindInTo {
 
     public static boolean find(DirectorEmployeeImpl employee, String name) {
         boolean select = false;
+
         if (!employee.getPersonalOnDirector().isEmpty()) {
             List<Employee> personalOnDirector1 = employee.getPersonalOnDirector();
+
             for (int counter = 0; counter < personalOnDirector1.size(); counter++) {
                 if (personalOnDirector1.get(counter).getPosition() == Position.DIRECTOR) {
-                    DirectorEmployeeImpl directorInTo = (DirectorEmployeeImpl) personalOnDirector1.get(counter);
-                    if (!directorInTo.getPersonalOnDirector().isEmpty()) {
-                        select = find(directorInTo, name);
-                    }
+                    select = find((DirectorEmployeeImpl) personalOnDirector1.get(counter), name);
                 } else {
                     System.out.print(name + " ?= " + personalOnDirector1.get(counter).getFirstName());
                     if (name.equals(personalOnDirector1.get(counter).getFirstName())) {
                         select = true;
-                        return select;
                     } else {
                         System.out.println("\t(NO)");
                     }
