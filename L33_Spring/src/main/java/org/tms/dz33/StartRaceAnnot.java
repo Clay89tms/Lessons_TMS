029d4238a13f00b8617cc398a988e3baf1ca3147
Clay89TMS <bboyClay89@gmail.com>
2023-02-21 16:50:42 +0300

    L34 Spring Aspect com5

--- L33_Spring/src/main/java/org/tms/dz33/StartRaceAnnot.java
+++ L33_Spring/src/main/java/org/tms/dz33/StartRaceAnnot.java
@@ -3,22 +3,23 @@ package org.tms.dz33;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.tms.dz33.service.ControlService;
+import org.tms.dz33.service.PrintService;
 
 import java.util.Scanner;
 
 public class StartRaceAnnot {
     public void start(){
         AbstractApplicationContext context = new AnnotationConfigApplicationContext("org.tms.dz33");
-        ControlService beanControlService = context.getBean(ControlService.class);
+        PrintService beanPrintService = context.getBean(PrintService.class);
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("Welcome to bet of sports!\n");
-            startRace(beanControlService, scanner);
+            startRace(beanPrintService, scanner);
         } catch (Exception exception) {
             System.out.println("exep" + exception);
         }
     }
 
-    private void startRace(ControlService beanControlService, Scanner scanner) {
+    private void startRace(PrintService beanControlService, Scanner scanner) {
         if (beanControlService.menuControlService(scanner)) {
             startRace(beanControlService, scanner);
         }
