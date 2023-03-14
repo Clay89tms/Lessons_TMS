7fef04065bcf1a0a84a7d0040ff4cac143183403
Clay89TMS <bboyClay89@gmail.com>
2023-03-14 15:16:47 +0300

    L35 Spring WEB Homework NEW com8

--- L33_Spring/src/main/java/org/tms/dz33/config/WebConfig.java
+++ L33_Spring/src/main/java/org/tms/dz33/config/WebConfig.java
@@ -1,11 +1,13 @@
-package org.tms.dz33.config;
+package org.tms.dz33.dto;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig {
 
     @Bean
