85b9e654446bc6ec9f4cdeb0c1c0e96489fd83b0
Clay89TMS <bboyClay89@gmail.com>
2023-03-15 13:37:01 +0300

    L42 Spring Boot Classwork com1.11

---
+++ L42_spring-boot-tms/src/main/java/org/tms/boot/SpringBootTmsApplication.java
@@ -0,0 +1,14 @@
+package org.tms.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTmsApplication.class, args);
+
+    }
+
+}
